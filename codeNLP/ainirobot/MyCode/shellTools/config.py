c7cc8ec9f52df496d0c8745784d779475679d4c9
lgy <liguoyu@ainirobot.com>
2017-09-23 16:01:51 +0800

    NLP

--- codeNLP/ainirobot/MyCode/shellTools/config.py
+++
@@ -1,3 +0,0 @@
-ResultFilePath = '/Users/orion/PycharmProjects/Test2/Data/Result/'
-RegularsFilePath = '/Users/orion/lgy/chat_system/chatter/Data/Common/Regulars/'
-InputDataFilePath = '/Users/orion/PycharmProjects/Test2/Data/'
